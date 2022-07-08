5481b5eb0aed377d298b62a94bfcde5b8465a951
Davi Luiz <davilucena222@gmail.com>
2022-07-08 16:49:08 -0300

    typescript settings updated

---
+++ src/components/ModalAddFood/index.tsx
@@ -0,0 +1,49 @@
+import { createRef, useRef } from 'react';
+import { FiCheckSquare } from 'react-icons/fi';
+
+import { Form } from './styles';
+import Modal from '../Modal';
+import Input from '../Input';
+import { FormHandles } from '@unform/core';
+
+interface DataProps {
+  image: string;
+  name: string;
+  price: string;
+  description: string;
+}
+
+interface ModalAddProps {
+  isOpen: boolean;
+  setIsOpen: () => void;
+  handleAddFood: (data: DataProps) => void;
+}
+
+export default function ModalAddFood({ isOpen, setIsOpen, handleAddFood }: ModalAddProps) {
+  const formRef = useRef<FormHandles>(null);
+
+  async function handleSubmit(data: DataProps) {
+    handleAddFood(data);
+    setIsOpen();
+  }
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <h1>Novo Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
+
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
+
+        <Input name="description" placeholder="Descrição" />
+        <button type="submit" data-testid="add-food-button">
+          <p className="text">Adicionar Prato</p>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+}
